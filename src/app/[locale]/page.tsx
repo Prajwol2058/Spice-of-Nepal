68b215049d64e00f7bded585cc717c028863821e
Prajwol Bakhati <59197402+Prajwol2058@users.noreply.github.com>
2025-03-21 11:20:38 +0545

    Add footer in mainpage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,8 @@
 import AboutUs from "@/components/AboutUs";
 import Navbar from "@/components/Header";
 import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
@@ -8,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <AboutUs />
+      <Footer />
     </>
   );
 }
