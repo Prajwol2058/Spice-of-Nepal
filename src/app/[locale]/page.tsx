702ab2455ca2b00564f92917a285d94f9659020d
Prajwol2058 <prajwol.bakhati@live.com>
2025-03-22 15:51:29 +0545

    chore: add navbar and footer to root layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,11 @@
 import AboutUs from "@/components/AboutUs";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <AboutUs />
-      <Footer />
     </>
   );
 }
